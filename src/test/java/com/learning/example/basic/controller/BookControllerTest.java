beb23434003a751f935a3d6f497fe24cad7e199a
pijush2000 <pijush2000@gmail.com>
2020-07-11 23:54:49 +0200

    Second working Commit

--- src/test/java/com/learning/example/basic/controller/BookControllerTest.java
+++ src/test/java/com/learning/example/basic/controller/BookControllerTest.java
@@ -1,8 +1,15 @@
 package com.learning.example.basic.controller;
 
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +24,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -45,12 +54,13 @@ public class BookControllerTest {
     public void init() {
         Book book = new Book(1L, "A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41"));
         when(mockRepository.findById(1L)).thenReturn(Optional.of(book));
+        //when(mockRepository.save(any(Book.class))).thenReturn(book);
     }
     
     
-    @WithMockUser(username = "user")
+    @WithMockUser(username = "user",roles={"USER"})
     @Test
-    public void find_authentication_ok() throws Exception {
+    public void find_authenticated_ok() throws Exception {
 
         mockMvc.perform(get("/books/1"))
                 .andDo(print())
@@ -61,11 +71,64 @@ public class BookControllerTest {
                 .andExpect(jsonPath("$.price", is(15.41)));
     }
     
+    
     @Test
-    public void find_authentication_401() throws Exception {
+    public void find_not_authenticated_401() throws Exception {
         mockMvc.perform(get("/books/1"))
                 .andDo(print())
                 .andExpect(status().isUnauthorized());
+    }
+
+    /*
+        {
+            "timestamp":"2019-03-05T09:34:13.280+0000",
+            "status":400,
+            "errors":["Author is not allowed.","Please provide a price","Please provide a author"]
+        }
+     */
+    @WithMockUser(username="admin",roles={"USER","ADMIN"})
+    @Test
+    public void save_emptyAuthor_emptyPrice_400() throws Exception {
+
+        String bookInJson = "{\"name\":\"ABC\"}";
+
+        mockMvc.perform(post("/books")
+                .content(bookInJson)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp", is(notNullValue())))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors", hasSize(3)))
+                .andExpect(jsonPath("$.errors", hasItem("Author is not allowed.")))
+                .andExpect(jsonPath("$.errors", hasItem("Please provide a author")))
+                .andExpect(jsonPath("$.errors", hasItem("Please provide a price")));
+    }
+
+    /*
+        {
+            "timestamp":"2019-03-05T09:34:13.207+0000",
+            "status":400,
+            "errors":["Author is not allowed."]
+        }
+     */
+    @WithMockUser(username="superadmin",roles={"USER","ADMIN","SUPER_ADMIN"})
+    @Test
+    public void save_invalidAuthor_400() throws Exception {
+
+        String bookInJson = "{\"name\":\" Spring REST tutorials\", \"author\":\"abc\",\"price\":\"9.99\"}";
+
+        mockMvc.perform(post("/books")
+                .content(bookInJson)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp", is(notNullValue())))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors", hasItem("Author is not allowed.")));
     }
     
     @SuppressWarnings("unused")
